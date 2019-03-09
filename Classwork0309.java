e1f2b3abd94cde75dd08773f6c1c0ab81af5a3fc
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-09 10:12:47 -0500

    helper method exchange created!

--- Classwork0309.java
+++ Classwork0309.java
@@ -1,5 +1,11 @@
 public class Classwork0309{
 
-  public int partition(int start, int end, int[] data)
+  public int partition(int start, int end, int[] data){
+
+  }
+
+  public int exchange(int num1, int num2, int[] data){
+    
+  }
 
 }
