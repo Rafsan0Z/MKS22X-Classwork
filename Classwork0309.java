722bcf30f59e899e3081b8d018368a87ba222e55
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-09 10:27:50 -0500

    exchange method completed!

--- Classwork0309.java
+++ Classwork0309.java
@@ -4,8 +4,10 @@ public class Classwork0309{
 
   }
 
-  public int exchange(int num1, int num2, int[] data){
-    
+  public void exchange(int num1, int num2, int[] data){ // these are indices
+    int temp = data[num1]
+    data[num1] = data[num2];
+    data[num2] = temp;
   }
 
 }
