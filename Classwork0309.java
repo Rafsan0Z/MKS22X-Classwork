b3a7fc4906e412e40337774e728cd96ad94df97d
RoboticsRafsan01 <mzaman1@stuy.edu>
2019-03-09 11:19:55 -0500

    more tests!

--- Classwork0309.java
+++ Classwork0309.java
@@ -43,7 +43,7 @@ public class Classwork0309{
 
 public static void main(String[] args){
 
-  int[] data = new int[] {8,6,7,5,3,0,9};
+  int[] data = new int[] {8,6,7,5,3,0,9,10,21,2,14};
   int result = partition(0,6,data);
   System.out.println(result);
   System.out.println(printArray(data));
